b0d14aeffeaff24df4003356806cfe976bec99d6
John Boiles <johnaboiles@gmail.com>
2016-03-28 16:00:20 -0700

    Cleaning up. Simplified threading complexity a bit.

--- recorder.py
+++ recorder.py
@@ -1,8 +1,16 @@
-import io
 import picamera
 import time
 import threading
 import subprocess
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+class AVConvException(Exception):
+    pass
 
 
 class StoppableThread(threading.Thread):
@@ -21,63 +29,89 @@ class StoppableThread(threading.Thread):
 
 
 class Recorder(object):
-    save_filename = None
-    directory = "videos/"
+    camera = None
+    stream = None
+    wait_recording_thread = None
 
-    def __init__(self, framerate=30, resolution=(1280, 720)):
-        self.running = True
+    def __init__(self, framerate=90, resolution=(640, 480), display_framerate=30, directory="videos/", bitrate=1700000, seconds=8):
         self.framerate = framerate
+        self.display_framerate = display_framerate
         self.resolution = resolution
+        self.directory = directory
+        self.bitrate = bitrate
+        self.seconds = seconds
+        self.lock = threading.Lock()
 
-    def stop(self):
-        self.running = False
+    def start_recording(self):
+        self.camera = picamera.PiCamera(resolution=self.resolution, framerate=self.framerate)
+        self.stream = picamera.PiCameraCircularIO(self.camera, seconds=self.seconds, bitrate=self.bitrate)
+        self._start_recording()
+        self.wait_recording_thread = StoppableThread(target=self.check_for_error_loop)
+        self.wait_recording_thread.daemon = True
+        self.wait_recording_thread.start()
 
-    def write_video(self, stream, filename="tmp"):
-        # Write the entire content of the circular buffer to disk. No need to
-        # lock the stream here as we're definitely not writing to it
-        # simultaneously
-        h264_filename = filename + ".h264"
-        with io.open(h264_filename, 'wb') as output:
-            for frame in stream.frames:
-                if frame.frame_type == picamera.PiVideoFrameType.sps_header:
-                    stream.seek(frame.position)
-                    break
-            while True:
-                buf = stream.read1()
-                if not buf:
-                    break
-                output.write(buf)
+    def _start_recording(self):
+        self.camera.start_recording(self.stream, format='h264', bitrate=self.bitrate, quality=0)
+        logger.info("Camera started")
 
-        output.close()
-        # avconv -r 30 -i 1458866583.h264 -vcodec copy 1458866583.mp4
-        mp4_filename = filename + ".mp4"
-        subprocess.call(["avconv", "-r", str(self.framerate), "-i", h264_filename, "-vcodec", "copy", mp4_filename])
+    def stop_recording(self):
+        self.wait_recording_thread.stop()
+        self._stop_recording()
 
-        # Wipe the circular stream once we're done
-        stream.seek(0)
-        stream.truncate()
-        return filename
+    def _stop_recording(self):
+        self.camera.stop_recording()
+        logger.info("Camera stopped")
+
+    def close(self):
+        self.stop_recording()
+        self.camera.close()
+        logger.info("Camera closed")
+
+    def write_video(self, stream, filename="tmp.mp4"):
+        # Spawn a transcoding process, mostly to encapsulate the stream
+        avconv_cmd = ["avconv", "-r", str(self.framerate), "-i", "-", "-vcodec", "copy", "-threads", "auto", "-r",
+                      str(self.display_framerate), filename]
+        logger.info("Starting avconv process with command: %s" % ' '.join(avconv_cmd))
+        process = subprocess.Popen(avconv_cmd, stdin=subprocess.PIPE, stdout=sys.stdout, stderr=sys.stderr)
+
+        # Find the first sps header frame in the video (this is a good place to split h264 streams)
+        for frame in stream.frames:
+            if frame.frame_type == picamera.PiVideoFrameType.sps_header:
+                stream.seek(frame.position)
+                break
+
+        while True:
+            buf = stream.read1()
+            if not buf:
+                break
+            process.stdin.write(buf)
+
+        process.stdin.close()
+        process.wait(10)
+        if process.returncode is None:
+            logger.error("avconv command did not end in time")
+            raise AVConvException()
+        elif process.returncode != 0:
+            logger.error("avconv exited with non-zero return code: %d" % process.returncode)
+            raise AVConvException()
 
     def save(self):
-        filename = "%.0f" % time.time()
-        self.save_filename = filename
-        return filename + ".mp4"
+        with self.lock:
+            logger.info('Saving replay')
+            filename = "%.0f.mp4" % time.time()
+            filepath = self.directory + filename
+            # Catch 0.5s after the goal
+            self.camera.wait_recording(0.5)
+            self.stop_recording()
+            self.write_video(self.stream, filepath)
+            logger.info('Saved replay to %s' % filepath)
+            self._start_recording()
 
-    def loop(self):
-        with picamera.PiCamera() as camera:
-            camera.resolution = self.resolution
-            camera.framerate = self.framerate
-            stream = picamera.PiCameraCircularIO(camera, seconds=1)
-            camera.start_recording(stream, format='h264')
+        return filename
 
-            try:
-                while self.running:
-                    camera.wait_recording(0.1)
-                    if self.save_filename is not None:
-                        # Write the 10 seconds "before" motion to disk as well
-                        self.write_video(stream, self.directory + self.save_filename)
-                        print('Saved replay for %s!' % self.save_filename)
-                        self.save_filename = None
-
-            finally:
-                camera.stop_recording()
+    def check_for_error_loop(self):
+        """This should be called periodically to check for exceptions"""
+        while not self.wait_recording_thread.stopped():
+            with self.lock:
+                self.camera.wait_recording()
+            time.sleep(1)
--- server.py
+++ server.py
@@ -1,38 +1,74 @@
-import io
-import signal
-import recorder
-from flask import Flask, request, send_from_directory, Response
+import atexit
+import logging.config
+from flask import Flask, send_from_directory, Response
+from recorder import Recorder
 
-# set the project root directory as the static folder, you can set others.
-app = Flask(__name__, static_url_path='')
 
-recorder_handler = recorder.Recorder()
-recorder_thread = None
-host = "http://10.0.0.58"
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s(%(module)s): %(message)s'
+        },
+    },
+
+    'handlers': {
+        # 'file': {
+        #     'level': 'DEBUG',
+        #     'class': 'logging.FileHandler',
+        #     'filename': '/var/logs/replaycam.log',
+        #     'formatter': 'verbose',
+        # },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'stream': 'ext://sys.stderr'
+        },
+    },
+
+    'loggers': {
+        '': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        },
+    }
+}
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger('__name__')
+
+app = Flask(__name__)
+app.debug = True
+
+video_directory = "/var/www/replaycam/videos/"
+recorder = Recorder(framerate=42, resolution=(1296, 972), display_framerate=21, directory=video_directory)
+recorder.start_recording()
+
+
+def cleanup():
+    recorder.close()
 
 
 @app.route('/videos/<path:path>')
 def videos(path):
-    return send_from_directory('videos', path)
+    return send_from_directory(video_directory, path)
 
 
 @app.route('/save')
 def save():
-    filename = recorder_handler.save()
-    path = host + "/videos/" + filename
-    return Response(path , mimetype='text/plain')
-
-
-def cleanup():
-    recorder_handler.stop()
-    recorder_thread.stop()
+    filename = recorder.save()
+    path = "/videos/" + filename
+    return Response(path, mimetype='text/plain')
 
 
 if __name__ == "__main__":
-    recorder_thread = recorder.StoppableThread(target=recorder_handler.loop)
-    recorder_thread.daemon = True
-    recorder_thread.start()
+    app.run(host='0.0.0.0', port=80, debug=True)
 
-    app.run(host='0.0.0.0', port=80)
 
-    cleanup()
+atexit.register(cleanup)
